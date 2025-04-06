0c794ac526668ba760a57f75eb79993b80b75633
joao.leandro <joao.leandro@tjam.jus.br>
2025-04-06 13:47:48 -0400

    Updates

---
+++ precache-manifest.09d10db6ac91053215f321e13b73bf49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b137e7022fa908d7488ccfea0120c9e",
+    "url": "/conviteCasamento/index.html"
+  },
+  {
+    "revision": "e062665bca8873290769",
+    "url": "/conviteCasamento/static/css/main.4a79494d.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/conviteCasamento/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "cb156245ee93a85eed33",
+    "url": "/conviteCasamento/static/js/3.44b60ca4.chunk.js"
+  },
+  {
+    "revision": "e062665bca8873290769",
+    "url": "/conviteCasamento/static/js/main.85ac6917.chunk.js"
+  },
+  {
+    "revision": "c23367bf21a4621b1db1",
+    "url": "/conviteCasamento/static/js/runtime~main.c274b947.js"
+  },
+  {
+    "revision": "8fcaf56951aa1be5ed60b5eb89ea623a",
+    "url": "/conviteCasamento/static/media/FontsFree-Net-lawrence-regular.8fcaf569.woff2"
+  },
+  {
+    "revision": "6fc220550b65fd4aead3644ab5888a2c",
+    "url": "/conviteCasamento/static/media/background.6fc22055.jpg"
+  }
+]);
